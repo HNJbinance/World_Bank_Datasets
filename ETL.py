3dc80e2beaa6b02c08b24ac2a96c0d2df04b2494
Hamza ENNAJI <hamzaonbinance@gmail.com>
2022-12-06 10:55:55 +0100

    extraction finished

--- ETL.py
+++ ETL.py
@@ -2,7 +2,12 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
+from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+import sqlite3
+import requests
 
+'''E-xtract'''
 '''T-ransform'''
 for dirname, _, filenames in os.walk('./'):
     for filename in filenames:
@@ -52,3 +57,53 @@ df_population = df_population.drop('Unnamed: 62', axis=1)
 # The purpose is to see what rows contain null values now that
 #   'Unnamed: 62' was dropped from the data.
 df_population[df_population.isnull().any(axis=1)]
+
+'''Extract from JSON and XML'''
+# Read in the population_data.json file using pandas's
+# read_json method. Don't forget to specific the orient option
+# store the results in df_json
+
+df_json = pd.read_json('./population_data.json', orient='records')
+print(df_json.head())
+
+# open the population_data.xml file and load into Beautiful Soup
+with open("./population_data.xml") as fp:
+    soup = BeautifulSoup(fp, features="html5lib") #lxml is the parser type
+
+# output the first 5 records in the xml file
+# this is an example of how to navigate the XML document with BeautifulSoup
+i = 0
+# use the find_all method to get all record tags in the document
+for record in soup.find_all('record'):
+    # use the find_all method to get all fields in each record
+    i += 1
+    for record in record.find_all('field'):
+        print(record['name'], ': ' , record.text)
+    print()
+    if i == 5:
+        break
+'''Extract from database'''
+# DEFINE THE DATABASE CREDENTIALS
+
+conn = sqlite3.connect('./population_data.db')
+
+print(pd.read_sql('SELECT "Country_Name", "Country_Code", "1960" FROM population_data', conn))
+
+# We can perform the same extraction from DB using sqlalchemy if using Mysql or Postgres
+
+'''Extract with api'''
+# Run the code example below to request data from the World Bank Indicators API. According to the documntation,
+# you format your request url like so:
+# http://api.worldbank.org/v2/countries/ + list of country abbreviations separated by ; + /indicators/ + indicator name + ? + options
+
+url = 'http://api.worldbank.org/v2/countries/br;cn;us;de/indicators/SP.POP.TOTL/?format=json&per_page=1000'
+r = requests.get(url)
+
+pd.DataFrame(r.json()[1])
+
+# TODO: get the url ready
+url = 'http://api.worldbank.org/v2/country/CH/indicator/SP.POP.TOTL/?format=json&date=1995:2001'
+
+# TODO: send the request
+r = requests.get(url)
+r.json()
